b386ff99693b6775a8f5bdb41203cd1a27ccb0c7
kivit2kk <martinkivi93@gmail.com>
2023-03-06 18:07:18 +0200

    creat new app

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// This is a comment to my code
+
+
+//rakendus küsib kasutajal sisestada oma nime
+//rakendus tervitab kasutaja nimepidi
+
+Console.WriteLine("Enter you name"); // Output
+//sõne - string 
+string userName = Console.ReadLine(); //input
+
+Console.WriteLine("Hello" + ","+userName+"!"); // Output
+
+//sring interpolation
+
+Console.WriteLine($"Hello, {userName} ! "); //Output
+
